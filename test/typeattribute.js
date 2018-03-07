b615c4e55adaecd241320ad4b364a9db19cf8390
Johann du Toit <exxonno@gmail.com>
2018-03-07 03:46:56 +0200

    Initial commit

---
+++ test/typeattribute.js
@@ -0,0 +1,16 @@
+// modules
+const assert      = require('assert');
+const _           = require('underscore');
+const fs          = require('fs');
+const passmarked  = require('passmarked');
+
+// handle the settings
+describe('lighthouse', function() {
+});
+
+
+
+
+
+
+
